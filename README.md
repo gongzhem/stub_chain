4c42ab978391abf8709c69caccdba35230660b3d
Zhen GONG <zhengong@sjtu.edu.cn>
2025-08-12 23:08:02 +0800

    new file:   .assets/fig02(a).png 	new file:   .assets/fig02(b-d).png 	new file:   .assets/fig03.png 	new file:   .assets/fig04.png 	new file:   .assets/fig05.png 	new file:   .assets/fig06.png 	modified:   README.md 	new file:   data/fig03_abtype_obc.h5 	new file:   data/fig04_spectral_rht_power.h5 	new file:   data/fig05_rht_along_chain.h5 	new file:   data/fig06_rht_scaling.h5 	modified:   src/fig03.py 	modified:   src/fig04.py 	modified:   src/fig05.py 	modified:   src/fig06.py

--- README.md
+++ README.md
@@ -16,3 +16,29 @@
 ### FIG. 1: Band structure of the stub lattice under periodic boundary condition
 
 ![Figure 1](.assets/fig01.png)
+
+### FIG. 2: Eigenstates and states in the upper/middle/lower band in OP mode under open boundary conditions
+
+(a) The band structure under OBCs.
+![Figure 2a](.assets/fig02(a).png)
+
+(b-d) Dipole moment distribution of the states in upper, middle and lower bands in OP mode.
+![Figure 2b-d](.assets/fig02(b-d).png)
+
+
+### FIG. 3: Band structures and dipole moment distributions of the A/B type stub chain
+
+![Figure 3](.assets/fig03.png)
+
+### FIG. 4: Spectral RHT between left- and right-end NPs.
+![Figure 4](.assets/fig04.png)
+
+### FIG. 5: RHT between the left-end NP and the remaining NPs
+![Figure 5](.assets/fig05.png)
+
+
+### FIG. 6: RHT power between two ends of the A/B-type stub chain with respect to the chain length under
+![Figure 6](.assets/fig06.png)
+
+
+### FIG. 7
--- src/fig03.py
+++ src/fig03.py
@@ -8,7 +8,7 @@
 @Desc    :   This script is used for plot the eigenfrequency spectrum for A/B-type stub chain, and the dipole moment distribution of CLS
 """
 
-# %% Packages import
+# %% Import Packages
 import os
 import sys
 from pathlib import Path
@@ -27,13 +27,100 @@ sns.set_theme("paper", style="ticks")
 with open(proj_root / "src/alias.yml") as f:
     alias = yaml.safe_load(f)
 
-# %% (b)
 
+# %% Load data
+datapath = proj_root / "data/fig03_abtype_obc.h5"
+dataset = {}
+with h5py.File(datapath, "r") as hf:
+    for key, gp in hf.items():
+        dataset[key] = {}
+        for key_sub, val in gp.items():
+            dataset[key][key_sub] = val[()]
 
-# %% (e)
+# %% Plot (b-f)
+um = 1e-6
+fig, axs = plt.subplots(2, 2, sharey="row")
+chain_types = ["SA", "SB"]
+stub_a = dataset["SA"]
+stub_b = dataset["SB"]
+# 1. Band structures
+clim = [
+    np.min(np.concat((stub_a["ipr_z"], stub_b["ipr_z"]))),
+    np.max(np.concat((stub_a["ipr_z"], stub_b["ipr_z"]))),
+]
+for ax, key in zip(axs[0], chain_types):
+    xdata = np.arange(dataset[key]["ipr_z"].size) + 1
+    ydata = dataset[key]["w_z[cm^-1]"]
+    cdata = dataset[key]["ipr_z"]
+    sc = ax.scatter(
+        xdata,
+        ydata.real,
+        c=cdata,
+    )
+    ax.set(
+        title=f"Spectrum ({key[-1]}-type)",
+        xlabel=alias["sn"],
+        xlim=[0, xdata.size],
+    )
+    cb = fig.colorbar(sc)
+    cb.ax.set(title="IPR")
+axs[0, 0].set(ylabel=alias["eig_wn"])
 
-
-# %% (c)
-
-
-# %% (f)
+# The dipole moment distribution
+for ax, key in zip(axs[1], chain_types):
+    rlist = dataset[key]["rlist"]
+    xdata = rlist[0]
+    Ntot = rlist.shape[1]
+    Ncell = Ntot // 3 + 1
+    sn = 70
+    ydata = np.abs(dataset[key]["v_z"][:, sn])
+    if key == "SA":
+        idx_A = list(np.arange(Ncell) * 3)
+        idx_B = list(np.arange(Ncell - 1) * 3 + 1)
+        idx_C = list(np.arange(Ncell - 1) * 3 + 2) + [-1]
+    elif key == "SB":
+        idx_A = list(np.arange(Ncell - 1) * 3 + 1)
+        idx_B = [0] + list(np.arange(Ncell - 1) * 3 + 2)
+        idx_C = list(np.arange(Ncell - 1) * 3 + 3)
+    markevery = 2
+    ax.plot(
+        xdata[idx_A] / um,
+        ydata[idx_A],
+        label="NP $A$",
+        c="#a8a6a7",
+        ls="--",
+        marker="^",
+        markevery=markevery,
+    )
+    ax.plot(
+        xdata[idx_B] / um,
+        ydata[idx_B],
+        label="NP $B$",
+        c="#b1283a",
+        ls="--",
+        marker="s",
+        markevery=markevery,
+    )
+    ax.plot(
+        xdata[idx_C] / um,
+        ydata[idx_C],
+        label="NP $C$",
+        c="#006a8e",
+        ls="--",
+        marker="x",
+        markevery=markevery,
+    )
+    ax.legend(
+        ncol=3,
+        fontsize="small",
+        loc=9,
+    )
+    ax.set(
+        xlim=[np.min(xdata) / um, np.max(xdata) / um],
+        ylim=[0, 0.25],
+        yticks=0.05 * np.arange(6),
+        xlabel=r"x (um)",
+        title=f"Dipole Moment Distribution ({key[-1]}-type)",
+    )
+axs[1, 0].set(ylabel="Nomalized Dipole Moment")
+fig.tight_layout()
